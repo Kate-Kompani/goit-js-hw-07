92175c607a9bd449b674b565d46714158dd6a926
Kateryna Kompaniiets <ketkacompani@gmail.com>
2021-09-11 07:39:30 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -23,11 +23,27 @@ const ingredients = [
 // list.insertAdjacentHTML('afterbegin', '<li>'ingredients[0]'</li>');
 // list.insertAdjacentHTML('beforeend', '<li>beforeend item</li>');
 // list.insertAdjacentHTML('afterend', '<p>afterend текст</p>');
-const items = ingredients.map((ingredient => {
-  const item = document.createElement("li");
-  item.textContent = ingredient;
-    return item;
-    console.log(item);
-}));
+// const items = ingredients.map((ingredient => {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//     return item;
+//     console.log(item);
+// }));
 
-console.log(item);
+
+
+function makeArray(firstArray, secondArray, maxLength) {
+    // Change code below this line
+  const newArray = firstArray.concat(secondArray);
+  console.log(newArray);
+if (newArray.length > maxLength) {
+  const a = newArray.slice(maxLength);
+  console.log(a);
+}
+  return newArray;
+
+
+    // Change code above this line
+  }
+
+makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 6)
